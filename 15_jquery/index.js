59bd511f6f80ee45940886c0885eb90d13985b85
tom474 <emsignouc0407@gmail.com>
2024-06-19 16:25:54 +0700

    Done jQuery learning

---
+++ 15_jquery/index.js
@@ -0,0 +1,69 @@
+// $("h1").addClass("big-title margin-50");
+
+// $("h1").text("Bye");
+
+// $("button").html("<em>Hey</em>");
+
+// $("img").attr("src");
+
+// $("a").attr("href", "https://yahoo.com");
+
+// $("h1").click(function () {
+// 	$("h1").css("color", "red");
+// });
+
+// $("button").click(function () {
+// 	$("h1").css("color", "purple");
+// });
+
+// $("input").keypress(function (event) {
+// 	$("h1").text(event.key);
+// });
+
+// $("h1").on("mouseover", function () {
+// 	$("h1").css("color", "purple");
+// });
+
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+// $("h1").append("<button>New</button>");
+// $("h1").prepend("<button>New</button>");
+// $("button").remove();
+
+// $("button").on("click", function () {
+// 	$("h1").hide();
+// });
+
+// $("h1").show();
+
+// $("button").on("click", function () {
+// 	$("h1").toggle();
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").fadeOut();
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").fadeToggle();
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").slideToggle();
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").animate({opacity: 0.5});
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").animate({margin: 20});
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").animate({margin: "20%"});
+// });
+
+// $("button").on("click", function () {
+// 	$("h1").slideUp().slideDown().animate({opacity: 0.5});
+// });
