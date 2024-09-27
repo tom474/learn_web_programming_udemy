7b948f244a7c1fa973eef6a3b82b48b31beea42e
tom474 <emsignouc0407@gmail.com>
2024-09-27 22:58:15 +0700

    Add initial React components and configuration files

---
+++ 23_reactjs/02_jsx_code_practice/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<>
+			<h1>My Favorite Foods</h1>
+			<ul>
+				<li>Bacon</li>
+				<li>Noodles</li>
+				<li>Beef Steak</li>
+			</ul>
+		</>
+	);
+}
+
+export default App;
