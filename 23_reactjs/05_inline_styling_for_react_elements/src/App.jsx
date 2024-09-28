2c6f2aa331920da75dbab9b8882e04cc9bab0613
tom474 <emsignouc0407@gmail.com>
2024-09-28 11:33:24 +0700

    Add inline styling for React elements

---
+++ 23_reactjs/05_inline_styling_for_react_elements/src/App.jsx
@@ -0,0 +1,17 @@
+const customStyle = {
+	color: "red",
+	fontSize: "20px",
+	border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
+function App() {
+	return (
+		<>
+			<h1 style={customStyle}>Hello World!</h1>
+		</>
+	);
+}
+
+export default App;
