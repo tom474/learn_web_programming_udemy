3c77f619c40aae0547e04d188aca032e04a27286
tom474 <emsignouc0407@gmail.com>
2024-10-07 15:30:35 +0700

    Complete 20_conditional_rendering

---
+++ 23_reactjs/20_conditional_rendering/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+const currentTime = new Date().getHours();
+
+function App() {
+	return (
+		<div className="container">
+			{/* Ternary Operator */}
+			{isLoggedIn ? <h1>Hello</h1> : <Login />}
+			{/* AND Operator */}
+			{currentTime > 12 && <h1>Why are you still working?</h1>}
+		</div>
+	);
+}
+
+export default App;
